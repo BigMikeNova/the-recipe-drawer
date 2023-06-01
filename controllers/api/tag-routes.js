677e4d14d8e4192e22a1a26d5cf2e155be083e87
BigMikeNova <125073452+BigMikeNova@users.noreply.github.com>
2023-05-31 20:15:37 -0400

    Revert "secure login"

--- controllers/api/tag-routes.js
+++ controllers/api/tag-routes.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
-const { RecipeTag, Recipe } = require('../../models');
+const { Tag, Product } = require('../../models');
 
-// The `/tags` endpoint
+// The `/api/tags` endpoint
 
-// GET /tags
+// GET /api/tags
 router.get('/', async (req, res) => {
     try {
-        const tagData = await RecipeTag.findAll({
-            include: [{ model: Recipe }],
+        const tagData = await Tag.findAll({
+            include: [{ model: Product }],
         });
         res.status(200).json(tagData);
     } catch (err) {
@@ -15,11 +15,11 @@ router.get('/', async (req, res) => {
     }
 });
 
-// GET one tag tags/1
+// GET one tag api/tags/1
 router.get('/:id', async (req, res) => {
     try {
-        const tagData = await RecipeTag.findByPk(req.params.id, {
-            include: [{ model: Recipe }],
+        const tagData = await Tag.findByPk(req.params.id, {
+            include: [{ model: Product }],
         });
         if (!tagData) {
             res.status(404).json({ message: 'No tag found with this id!' });
@@ -31,20 +31,20 @@ router.get('/:id', async (req, res) => {
     }
 });
 
-// POST create new tag tags
+// POST create new tag api/tags
 router.post('/', async (req, res) => {
     try {
-        const tagData = await RecipeTag.create(req.body);
+        const tagData = await Tag.create(req.body);
         res.status(200).json(tagData);
     } catch (err) {
         res.status(400).json(err);
     }
 });
 
-// PUT update tag tags/1
+// PUT update tag api/tags/1
 router.put('/:id', async (req, res) => {
     try {
-        const tagData = await RecipeTag.update(req.body, {
+        const tagData = await Tag.update(req.body, {
             where: { id: req.params.id },
         });
         if (!tagData[0]) {
@@ -57,10 +57,10 @@ router.put('/:id', async (req, res) => {
     }
 });
 
-// DELETE tag tags/1
+// DELETE tag api/tags/1
 router.delete('/:id', async (req, res) => {
     try {
-        const tagData = await RecipeTag.destroy({
+        const tagData = await Tag.destroy({
             where: { id: req.params.id },
         });
         if (!tagData) {
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,38 +1,6 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-// Get all users
-router.get('/', async (req, res) => {
-    try {
-        const dbUserData = await User.findAll({
-            attributes: { exclude: ['password'] },
-        });
-        res.status(200).json(dbUserData);
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
-    }
-});
-
-
-// Get a single user by ID withAuth
-router.get('/:id', withAuth, async (req, res) => {
-    try {
-        const dbUserData = await User.findByPk(req.params.id, {
-            attributes: { exclude: ['password'] },
-        });
-        if (!dbUserData) {
-            res.status(404).json({ message: 'No user found with this id!' });
-            return;
-        }
-        res.status(200).json(dbUserData);
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
-    }
-});
-
-
 // Create new user
 router.post('/', async (req, res) => {
     try {
