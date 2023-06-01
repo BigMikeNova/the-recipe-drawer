4d5cf9269fc11d61b1b0bfa9dcd6d242beb05bb7
jkrieger6 <jkrieger11@gmail.com>
2023-05-31 17:25:51 -0700

    added wuthauth to connect to server

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 // Get all users
 router.get('/', async (req, res) => {
