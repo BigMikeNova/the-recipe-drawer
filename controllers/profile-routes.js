c5f0589b25a75164df661d89453b5e2eb7b53b78
jkrieger6 <jkrieger11@gmail.com>
2023-06-05 18:51:57 -0700

    profile page now loads

--- controllers/profile-routes.js
+++ controllers/profile-routes.js
@@ -4,18 +4,19 @@ const withAuth = require('../utils/auth');
 
 // GET /recipes for logged in user
 router.get('/', withAuth, async (req, res) => {
+    console.log(req.session.user_id);
     try {
         // Find the logged in user based on the session ID
-        const userData = await User.findByPk(req.session.user.id, {
+        const userData = await User.findByPk(req.session.user_id, {
             attributes: { exclude: ['password'] },
             include: [{ model: Recipe }],
         });
         const user = userData.get({ plain: true });
-        res.render('profile', {
-            ...user,
-            logged_in: true
+        res.render('userprofile', {
+            logged_in: req.session.logged_in,
         });
     } catch (err) {
+        console.log(err);
         res.status(500).json(err);
     }
 });
